7420a4402799a99647fcdb4c180284038daa8f52
David Fleming <tonage72@gmail.com>
2023-11-18 12:57:50 -0500

    Commit before changes

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const shareButton = document.querySelector(".share");
 const personInfo = document.querySelector(".personinfo");
-const shareMenu = document.querySelector(".sharemenu")
+const shareMenu = document.querySelector(".sharemenu");
+let shownDiv = "person";
 
-shareButton.addEventListener('click', function() {
-	personInfo.style.display = "none";
-	shareMenu.style.display = "flex"
-})
+function switchDivs() {
+	if (shownDiv === "person") {
+		personInfo.style.display = "none";
+		shareMenu.style.display = "flex";
+		shownDiv = "share";
+	} else {
+		personInfo.style.display = "grid";
+		shareMenu.style.display = "none";
+		shownDiv = "person";
+	}
+	
+}
