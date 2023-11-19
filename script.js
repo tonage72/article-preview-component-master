4c8e360b81533836a8bd4880e7a4e728feca8004
David Fleming <tonage72@gmail.com>
2023-11-18 23:52:09 -0500

    Some fine tuning should do it.

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 const shareButton = document.querySelector(".share");
-const personInfo = document.querySelector(".personinfo");
 const shareMenu = document.querySelector(".sharemenu");
 let shownDiv = "person";
 
 function switchDivs() {
 	if (shownDiv === "person") {
-		personInfo.style.display = "none";
 		shareMenu.style.display = "flex";
 		shownDiv = "share";
 	} else {
-		personInfo.style.display = "grid";
 		shareMenu.style.display = "none";
 		shownDiv = "person";
 	}
-	
 }
